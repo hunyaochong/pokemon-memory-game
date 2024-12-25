9ef77d3c481817cfdb70d2b6eb0ce5e2b8442df0
ZephyRemix <hunyaochong@gmail.com>
2024-12-25 17:15:04 +0800

    cleaned up codebase

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "../Header";
-import AudioButton from "../AudioButton";
 import ScoreBoard from "../Scoreboard";
 import CardGrid from "../CardGrid";
 import PikachuBoop from "../PikachuBoop";
 import loadPokemons from "../../pokemonGenerator";
 import { shuffle, checkDistinct } from "../../utils";
-import pokemonMusic from "../../assets/pokemon.mp3";
 
 function App() {
-  /* 
-    Array of 24 pokemons (2rowsx4columns) -> 4 sets of variations
-    pokemonArtworkUrl = [
-    {name: "pikachu", artwork: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png"},
-    {name: "gastly", artwork: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png"},
-    ....
-    ]
-  */
   const [deck, setDeck] = React.useState([]);
   const [cardsInUse, setCardsInUse] = React.useState([]);
   const [score, setScore] = React.useState(0);
